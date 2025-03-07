eb4aaba538cf6cd0e4d787835bde9ded17cebe8a
sergioob4 <144851074+sergioob4@users.noreply.github.com>
2025-03-07 19:48:09 -0400

    Login

--- src/Vistas/Login.jsx
+++ src/Vistas/Login.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import Header from './components/header';
 
 export default function Login() {
   return(
-  <div>Login</div>
-  );
+    <div className="login-container">
+    {/* Formulario */}
+    <form className="formulario">
+      <h2 className="crear-cuenta">Iniciar sesión</h2>
+      <div className="iconos">
+        <div className="borde-iconos">
+          <img src="public/Imagenes/Google.png" atl ="Google"/>
+        </div>
+        
+        <div className="borde-iconos">
+          <img src="public/Imagenes/Facebook.png" alt = "Facebook"/>
+        </div>
+      </div>
+      <p className="cuenta-gratis">Se parte de la experiencia</p>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Contraseña" />
+      <input type="button" value="Iniciar Sesión" />
+    </form>
+
+    {/* Footer */}
+    <footer>
+      <p>© 2025 Ávila - UNIMET</p>
+    </footer>
+  </div>
+);
+  
+
 };
+"public/Imagenes/Facebook.png"
