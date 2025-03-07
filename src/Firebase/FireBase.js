6c1323c9e981a1f86b89a32d9d9e5a37b1ca860f
VictorBettelli <144615945+VictorBettelli@users.noreply.github.com>
2025-03-07 17:38:53 -0400

    Update FireBase.js

---
+++ src/Firebase/FireBase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCtTruY0EOCP-um2zuoxRKumi3Mbd5dIO8",
+
+  authDomain: "proyecto-final-3ff5a.firebaseapp.com",
+
+  projectId: "proyecto-final-3ff5a",
+
+  storageBucket: "proyecto-final-3ff5a.firebasestorage.app",
+
+  messagingSenderId: "759224117201",
+
+  appId: "1:759224117201:web:6c5833adf4570e1de262f0"
+
+};
+
+export const app = initializeApp(firebaseConfig);
