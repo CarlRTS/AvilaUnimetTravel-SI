64c1487458222014c65f5ac61e42430bd5d18463
Santiago Gomez Cornejo <131918914+Santos2705@users.noreply.github.com>
2025-03-20 19:40:04 -0400

    Update Footer.jsx

--- src/Vistas/components/Footer.jsx
+++ src/Vistas/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom"; // Importa Link para la navegación
 
 const Footer = () => (
   <footer>
     <div className="footer-content">
       <div className="social-logos">
-        <a 
+        <a
           href="https://www.instagram.com/unimet/"
-          target="_blank" 
-          rel="noopener noreferrer" 
+          target="_blank"
+          rel="noopener noreferrer"
           className="instagram-logo"
         >
           <img src="/Imagenes/instagram.webp" alt="Instagram" />
@@ -21,6 +22,24 @@ const Footer = () => (
         <a href="#" className="brand-logo">
           <img src="/Imagenes/proyectoavila.png" alt="Logo colaborador 2" />
         </a>
+      </div>
+
+      <div className="footer-button">
+        {/* Botón para ir a Información */}
+        <a href="/informacion" className="info-button">
+          ¡Descubre más sobre nosotros!
+        </a>
+
+        {/* Botón sencillo para ir a Contactos */}
+        <Link to="/contactos" className="contactanos-button">
+          Contáctanos
+        </Link>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer
       </div>
     </div>
   </footer>
