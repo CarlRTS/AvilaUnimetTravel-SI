97bdfdbb62e88d4a7cc6ab6e092aeba39538e7f6
CarlRTS <moreno.carlos@correo.unimet.edu.ve>
2025-03-20 20:32:30 -0400

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,16 @@ import EditPerfile from './Vistas/EditPerfile';
 import { MisReservas } from './Vistas/MisDestinos';
 import GestionRutas from './Vistas/GestionRutas';
 import Informacion from './Vistas/Informarcion';
+
+
+
+
+
+import Galeria from './Vistas/Galeria';
+import Reseñas from './Vistas/Reseñas';
+
 import searchbar from './Vistas/components/searchbar';
+
 
 
 // Componente para rutas protegidas
@@ -40,6 +49,15 @@ export default function App() {
           
           {/* Rutas públicas */}
           <Route path="/registrar" element={<Registro />} />
+
+
+=======
+          {/* Ruta de Galeria (pública) */}
+          <Route path="/galeria" element={<Galeria />} />
+
+          {/* Ruta Reseñas */}
+          <Route path="/reseñas" element={<Reseñas />} />
+
 
           {/* Rutas protegidas */}
           <Route path="/mi-perfil" element={
